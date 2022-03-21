a8d63d74dc85a43ba108b35813b3446a946b56b3
‘Gonzo <gonzonieto@gmail.com>
2022-03-21 04:29:17 +0000

    solved!

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  const flatArray = array.flat(Infinity);
+
+  return flatArray.length === 1
+    ? flatArray[0]
+    : flatArray[0] + sumItems(flatArray.slice(1));
+};
 
 module.exports = sumItems;
